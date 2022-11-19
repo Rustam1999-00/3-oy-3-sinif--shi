606b19c4a62f8f1c1a8998f8f92f50e7de8deac3
rustam-1999 <tolliboyevrustam1999@gmail.com>
2022-11-19 23:28:42 +0500

    dell innit

---
+++ Js/main.js
@@ -0,0 +1,9 @@
+var elbody=document.querySelector('body')
+var elbtne=document.querySelector('.btne')
+
+
+
+     elbtne.addEventListener("click",function(){       
+  elbody.classList.toggle('dark');
+})
+
